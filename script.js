1a4526d056dccb8c5fe8d5d3527dc3251c0dbbcb
Briqn <pitet.brian@gmail.com>
2017-10-08 23:30:55 +0200

    Ajout MAPS, vidoe, input text, upload image

--- script.js
+++ script.js
@@ -29,16 +29,16 @@ $(document).ready(function () {
 
     });
 
-/*    $('#AjoutModul').click(function () {
-        $('<div class="row"> <div id="modultext" class="col-md-12">module TextAdd <div id="AjoutModul" class="btn"><i class="fa fa-plus-square" aria-hidden="true"></i> </div><div id="removemodul" class="btn"><i class="fa fa-trash" aria-hidden="true"></i> </div> </div> </div>').insertAfter();
+    /*    $('#AjoutModul').click(function () {
+            $('<div class="row"> <div id="modultext" class="col-md-12">module TextAdd <div id="AjoutModul" class="btn"><i class="fa fa-plus-square" aria-hidden="true"></i> </div><div id="removemodul" class="btn"><i class="fa fa-trash" aria-hidden="true"></i> </div> </div> </div>').insertAfter();
 
-        // alert("Add bouton was clicked.");
+            // alert("Add bouton was clicked.");
 
-    });*/
+        });*/
 
     $(document).on('click', '#AjoutModul', function () {
-        var modul = $(this).parent();
-        $('<div class="row"> <div id="modultext" class="col-md-12">module TextAdd <div id="AjoutModul" class="btn"><i class="fa fa-plus-square" aria-hidden="true"></i> </div><div id="removemodul" class="btn"><i class="fa fa-trash" aria-hidden="true"></i> </div> </div> </div>').insertAfter(modul);
+        var modul = $(this).closest('.row');
+        $('<div class="row"> <div id="modultext" class="col-md-12">module TextAdd <input type="text" class="form-control" id="usr"><div id="AjoutModul" class="btn"><i class="fa fa-plus-square" aria-hidden="true"></i> </div><div id="removemodul" class="btn"><i class="fa fa-trash" aria-hidden="true"></i> </div> </div> </div>').insertAfter(modul);
 
 
 
@@ -67,9 +67,19 @@ $(document).ready(function () {
         $(this).closest('.row').addClass('col-md-6');
     });
 
-        $(document).on('click', '#delColumn', function () {
+    $(document).on('click', '#delColumn', function () {
         $(this).closest('.row').removeClass();
         $(this).closest('.row').addClass('col-md-12');
     });
 
 });
+
+
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
